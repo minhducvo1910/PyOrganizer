466e8e00cfadb8668eea705761dc953d4b1c11bd
Minh Duc Vo <paulthimony@gmail.com>
2026-06-08 15:55:09 -0500

    Add --organize to main

--- Main.py
+++ Main.py
@@ -5,23 +5,39 @@
 
 import argparse
 from Scanner import scan
+from Organizer import organize
 
 def main():
     parser = argparse.ArgumentParser(description="PyOrganizer - File Managemnet Tool")
     parser.add_argument("--scan", metavar="FOLDER", nargs="+", help= "Scan a folder and list files")
+
+    parser.add_argument("--organize", metavar="FOLDER", nargs="+", help= "Organize a folder")
+
     args = parser.parse_args()  #Read user input in the command line after main.py
 
+    #NOTE: Organize path
+    scan_folder = " ".join(args.scan) if args.scan else None                #Join words with space
+    organize_folder = " ".join(args.organize) if args.organize else None
+
     if args.scan:
-        folder = " ".join(args.scan)  #Join words with space
+       
         try:
-            results = scan(folder)
+            results = scan(scan_folder)
             for result in results:
                 print(result)
         except ValueError as e:
             print(f"Error: {e}")
+    elif args.organize:
+        try:
+            results = scan(organize_folder)
+            organize(results)
+        except ValueError as e:
+            print(F"Error: {e}")
     else:
         parser.print_help()
-        
+    
+    
+
 
 if __name__ == "__main__":
     main()
